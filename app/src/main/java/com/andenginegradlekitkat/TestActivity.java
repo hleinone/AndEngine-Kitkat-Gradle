259c90577f9ecdf81b9a842de32b5c8c2f77c845
Hannu Leinonen <hleinone@gmail.com>
2014-05-10 15:42:55 +0300

    Java-style code formatting.

--- app/src/main/java/com/andenginegradlekitkat/TestActivity.java
+++ app/src/main/java/com/andenginegradlekitkat/TestActivity.java
@@ -9,32 +9,28 @@ import org.andengine.entity.scene.background.Background;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 
 
-public class TestActivity extends SimpleBaseGameActivity
-{
-  private Camera camera;
-  private static final int CAMERA_WIDTH = 720;
-  private static final int CAMERA_HEIGHT = 480;
+public class TestActivity extends SimpleBaseGameActivity {
+    private Camera camera;
+    private static final int CAMERA_WIDTH = 720;
+    private static final int CAMERA_HEIGHT = 480;
 
-  @Override
-  public EngineOptions onCreateEngineOptions()
-  {
-    camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-    EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED,
-        new FillResolutionPolicy(), camera);
-    return engineOptions;
-  }
+    @Override
+    public EngineOptions onCreateEngineOptions() {
+        camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+        EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED,
+                new FillResolutionPolicy(), camera);
+        return engineOptions;
+    }
 
-  @Override
-  protected void onCreateResources()
-  {
+    @Override
+    protected void onCreateResources() {
 
-  }
+    }
 
-  @Override
-  protected Scene onCreateScene()
-  {
-    Scene scene = new Scene();
-    scene.setBackground(new Background(0.09804f, 0.6274f, 0.8784f));
-    return scene;
-  }
+    @Override
+    protected Scene onCreateScene() {
+        Scene scene = new Scene();
+        scene.setBackground(new Background(0.09804f, 0.6274f, 0.8784f));
+        return scene;
+    }
 }
